b85cae8309c54993cdc29d85c40491d1e422a9ef
Keenan Ng <keenandng@gmail.com>
2020-11-03 18:12:52 -0800

    user can view details of a product front-end | complete

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import PriceFormatter from './price-formatter';
 
 class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: null
+      product: null,
+      isLoading: true
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -14,7 +16,8 @@ class ProductDetails extends React.Component {
       .then(response => response.json())
       .then(productDetails => {
         this.setState({
-          product: productDetails
+          product: productDetails,
+          isLoading: false
         });
       })
       .catch(err => console.error(err));
@@ -25,15 +28,44 @@ class ProductDetails extends React.Component {
   }
 
   render() {
+    if (this.state.isLoading) return <h1>Loading Item</h1>;
+    const image = this.state.product.image;
+    const name = this.state.product.name;
+    const price = PriceFormatter(this.state.product.price);
+    const shortDescription = this.state.product.shortDescription;
+    const longDescription = this.state.product.longDescription;
     return (
+      <div className="card mb-3 product-item" style={{ maxWidth: 800 + 'px' }}>
+        <button
+          className="btn btn-light back-button ml-4 mt-3 mb-3"
+          onClick={this.handleClick}>
+          &lt; Back to catalog
+        </button>
+        <div className="row no-gutters justify-content-between">
+          <div className="col-sm-6">
+            <img src={image} className="card-img img-fit ml-4" alt={name} />
+          </div>
+          <div className="col-sm-5">
+            <div className="card-body">
+              <h5 className="card-title">{name}</h5>
+              <p className="card-subtitle text-secondary product-price mb-2">{price}</p>
+              <p className="card-text">{shortDescription}</p>
+            </div>
+          </div>
+
+          <div className="card-body col-sm-12 p-4">
+            <p>{longDescription}</p>
+          </div>
+        </div>
+      </div>
+    );
+    /* return (
       <div className="card mb-3" style={{ maxWidth: 540 + 'px' }}>
         <button
           className="btn btn-light back-button ml-4 mt-3"
-          // possibly change next line to use the props.setView instead?
           onClick={this.handleClick}>
             &lt; Back to catalog
         </button>
-        {/* <a href="#" className="links ml-4 mt-3 text-muted">&lt; Back to catalog</a> */}
         <div className="row no-gutters">
           <div className="col-sm-7">
             <img src={this.state.product.image} className="card-img img-fit" alt="ostrich pillow"/>
@@ -52,7 +84,7 @@ class ProductDetails extends React.Component {
 
         </div>
       </div>
-    );
+    ); */
 
   }
 }
--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -11,9 +11,13 @@ function ProductListItem(props) {
         src={props.product.image}
         alt={props.product.name}
         onClick={handleClick}
-        className="card-img-top img-fit"/>
+        className="card-img-top img-fit hover img-hover"/>
       <div className="card-body">
-        <h5 className="card-title mb-3">{props.product.name}</h5>
+        <h5
+          className="card-title mb-3 hover title"
+          onClick={handleClick} >
+          {props.product.name}
+        </h5>
         <p className="card-subtitle text-secondary product-price mb-2">
           {PriceFormatter(props.product.price)}
         </p>
