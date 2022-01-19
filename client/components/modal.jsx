65fddd630b9cb2d035bb0e8e70e7d1c41c8e7271
Keenan Ng <keenanng.dev@gmail.com>
2022-01-19 13:07:26 -0800

    webpack experimentation to fix my database errors, beginning changes for creating quantity feature for cart, fixed jquery source

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -18,17 +18,23 @@ function AddItemModal(props) {
       centered
     >
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
+        <Modal.Title id="contained-modal-title-vcenter" className="text-center">
           {props.product.name}
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <div className="container">
           <div className="row flex-row justify-content-between">
-            <h4 className="col-6">How many?</h4>
+            <h4 className="col-6 px-0">Confirm amount</h4>
             <form className="form-quantity col-6 row justify-content-end">
               <label className="m-0 align-self-center" htmlFor="quantity">Quantity:</label>
-              <input type="number" id="quantity" name="quantity" min="1" size="1"/>
+              <input
+                type="number"
+                id="quantity"
+                name="quantity"
+                min="1"
+                size="1"
+              />
             </form>
           </div>
         </div>
